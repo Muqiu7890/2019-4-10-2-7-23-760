48a5c2855140ebc4320fb19ac64e79553e333771
Muqiu7890 <1337651840@qq.com>
2019-05-24 02:01:43 +0800

    finish taxi fee

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    const {distance, parkTime} = inputs
+    let price = 0
+    if (distance > 0 && distance < 2) {
+        price = 6
+    } else if (distance <= 8) {
+        price = 6 + (distance - 2) * 0.8
+    } else {
+        price = 10.8 + (distance - 8) * 1.5 * 0.8
+    }
+    return Math.round(price + 0.25 * parkTime)
 };
